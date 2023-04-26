f0043b0fb8dc75c8ce1a14f767547c1762f64feb
IncognitoTmp-201 <131511137+IncognitoTmp-201@users.noreply.github.com>
2023-04-26 10:06:11 +0000

    Criação do projeto

---
+++ HybridBlazorSample/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HybridBlazorSample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HybridBlazorSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HybridBlazorSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
